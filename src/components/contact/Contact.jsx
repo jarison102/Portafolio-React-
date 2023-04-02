0114c7d582d8c1a1ccaa64b0d3835b12aa84e119
JARRIWON STIVEM MICAN CESPEDES <jarimices@gmail.com>
2023-04-02 16:43:47 -0500

    Todo terminado falta el formulario

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
 import './contact.css'
+import {MdAttachEmail} from 'react-icons/md'
+import {BsMessenger} from 'react-icons/bs'
+import {BsWhatsapp} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from  'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_w9dbt7h', 'template_h99u7dp', form.current, 'sYtgX2XyX7OTukoAR')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get in Touch</h5>
+      <h2>Contactame</h2>
+      <div className='container contact__container'>
+        <div className='contact__options'>
+        <article className='contact__option'>
+          <MdAttachEmail className='contact__option-icon'/>
+          <h4>Email</h4>
+          <h5>Jarimices@gmaill.com</h5>
+          <a href="mailto:jarimices@gmail.com" target='_blank'>Enviar Mensaje</a>
+        </article>
+
+        <article className='contact__option'>
+          <BsMessenger className='contact__option-icon'/>
+          <h4>Messenger</h4>
+          <h5>Jarison Stived</h5>
+          <a href="https://m.me/jarison.micancespedes" target='_blank'>Enviar Mensaje</a>
+        </article>
+
+        <article className='contact__option'>
+          <BsWhatsapp className='contact__option-icon'/>
+          <h4>Whatsapp</h4>
+          <h5>+57 3216692365</h5>
+          <a href="https://api.whatsapp.com/send?phone=573216692365" target='_blank'>Enviar Mensaje</a>
+        </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Tu nombre completo' required/>
+          <input type="email" name='email' placeholder='Tu Correo' required/>
+          <textarea name="message" id="7" placeholder='Tu mensaje' required></textarea>
+          <button type='submit' className='btn btn-primary'>Enviar</button>
+        </form>
+
+      </div>
+    </section>
   )
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import './footer.css'
+import {BsFacebook} from 'react-icons/bs'
+import {BsTwitter} from 'react-icons/bs'
+import {BsInstagram} from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+      <footer>
+      <a href="#" className='footer__logo'>Stived</a>  
+
+      <ul className='permalinks'>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#about">Sobre mi</a></li>
+        <li><a href="#experience">Experiencia</a></li>
+        <li><a href="#services">Servicios</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonios</a></li>
+        <li><a href="#contact">Contacto</a></li>
+      </ul>
+      
+      <div className='footer__socials'>
+        <a href="https://www.instagram.com/stived_t/" target="_blank"><BsInstagram/></a>
+        <a href="https://twitter.com/MicanStiven" target="_blank"><BsTwitter/></a>
+        <a href="https://www.facebook.com/jarison.micancespedes/" target="_blank"><BsFacebook/></a>
+      </div>
+      <div className='footer__copyright'>
+        <small>&copy; Stived Todos los derechos reservados</small>
+      </div>
+      </footer>
   )
 }
 
